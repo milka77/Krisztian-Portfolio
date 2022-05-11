a0defbe90fcd85238b087381d2d85cef64618883
milka77 <55999046+milka77@users.noreply.github.com>
2022-05-11 11:18:08 +0100

    Added emailJS functionality to the contact section, and states to clear form after submission with success or error message

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,43 +1,106 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react'
 import './contact.css'
 import { AiOutlineMail } from 'react-icons/ai'
-
+import emailjs from '@emailjs/browser'
 
 function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [formMessage, setFormMessage] = useState('')
+  const [formMessageClass, setFormMessageClass] = useState('')
+
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('gmail', 'rosie', form.current, 'user_v1yovIkE2muZxlMxPMJpU')
+      .then(
+        (result) => {
+          console.log(result.text)
+
+          setName('')
+          setEmail('')
+          setMessage('')
+          setFormMessage('Your Message Sent Succesfully')
+          setFormMessageClass('form-message-success')
+        },
+        (error) => {
+          console.log(error.text)
+          setFormMessage('Message not sent, please try again later')
+          setFormMessageClass('form-message-error')
+        }
+      )
+  }
+
   return (
     <section id='contact'>
-      <div className="container contact__container">
+      <div className='container contact__container'>
         <h5>Let's Talk</h5>
         <h2>Contact Me</h2>
 
-        <div className="contact__content">
-          <div className="contact__form">
-            <form action="">
+        <div id='form-message' className={ formMessageClass }>{ formMessage }</div>
+
+        <div className='contact__content'>
+          <div className='contact__form'>
+            <form ref={ form } onSubmit={ sendEmail }>
               <div className='form-control'>
-                {/* <label className='form-label' htmlFor="name">Name:</label> */}
-                <input className='form-item' type="text" name="name" placeholder='Enter Your Name'/>
+                <input
+                  className='form-item'
+                  type='text'
+                  name='name'
+                  placeholder='Enter Your Name'
+                  value={ name }
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
               </div>
               <div className='form-control'>
-                {/* <label className='form-label' htmlFor="name">Email:</label> */}
-                <input className='form-item' type="email" name="name" placeholder='Enter Your Email'/>
+                <input
+                  className='form-item'
+                  type='email'
+                  name='email'
+                  placeholder='Enter Your Email'
+                  value={ email }
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
               <div className='form-control'>
-                {/* <label className='form-label' htmlFor="message">Message:</label> */}
-                <textarea className='form-item' name="message" placeholder='Enter Your Message' rows={5}></textarea>
+                <textarea
+                  className='form-item'
+                  name='message'
+                  placeholder='Enter Your Message'
+                  rows={5}
+                  value={ message }
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
+                ></textarea>
               </div>
               <div className='submit'>
-                <button type="submit" className='btn btn-primary'>Submit <AiOutlineMail className='contact__icon'/></button>
+                <button type='submit' className='btn btn-primary'>
+                  Send Message <AiOutlineMail className='contact__icon' />
+                </button>
               </div>
             </form>
           </div>
 
-          <div className="contact__info">
+          <div className='contact__info'>
             <ul>
               <li>Have an project idea?</li>
               <li>Need a new website?</li>
               <li>Or just want to chat?</li>
             </ul>
-            <p>Use the form on the site or write an <a href='mailto:krisztian.keseru@gmail.com'>email <AiOutlineMail className='contact__icon'/></a> and I'll be in a touch with you.</p>
+            <p>
+              Use the form on the site or write an{' '}
+              <a href='mailto:krisztian.keseru@gmail.com'>
+                email <AiOutlineMail className='contact__icon' />
+              </a>{' '}
+              and I'll be in a touch with you.
+            </p>
             <h3>Thank You!</h3>
           </div>
         </div>
