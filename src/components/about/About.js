005368dbb68ae19a30e9def692f14fe6e4fbc137
milka77 <55999046+milka77@users.noreply.github.com>
2022-05-11 09:23:57 +0100

    Added content to the About section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './about.css'
+import Me from '../../assets/me.png'
+import CV from '../../assets/cv.pdf'
 
 function About() {
   return (
@@ -7,7 +9,25 @@ function About() {
       <div className="container about__container">
         <h5>About Me</h5>
         <h2>About Me</h2>
-        <p>Hello! My name is Krisztian</p>
+
+        <div className="about__content">
+          <div className="about__content-left">
+            <p>Hello! My name is Krisztian.</p>
+            <p>
+              I'm a tech enthusiast who found his real passion in coding. My interest in web development started in the early 2000s when I
+               met HTML and CSS. I was creating fun sites and the website for our computer shop. In 2020 I decided to take this hobby to the
+               next level and signed up for a Full-Stack Development course with Code Institute accredited by the Edinborough University.
+               After I receive my Level 5 Diploma in Software Development, I'm ready to face new challenges. I want to start my professional 
+               Developer career, I'm open to freelancing, Junior Developer positions or Internships. Feel free to <a href='#contact'>contact</a> me.
+            </p>
+            <div className='cv-button'>
+              <a href={ CV } className='btn btn-primary' target='_blank'  rel='noreferrer'>Download My CV</a>
+            </div>
+          </div>
+          <div className="about__content-right">
+            <img src={ Me } alt="Me" />
+          </div>
+        </div>
       </div>
     </section>
   )
